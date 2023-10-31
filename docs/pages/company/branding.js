5706c791213e8d6cf4aa99d15e6cc4acc5858815
Red <143008303+codemasterred@users.noreply.github.com>
2023-10-31 17:12:18 +0000

    Update branding.js
    Signed-off-by: Red <143008303+codemasterred@users.noreply.github.com>

--- docs/pages/company/branding.js
+++ docs/pages/company/branding.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import TopLayoutCareers from 'docs/src/modules/components/TopLayoutCareers';
 import * as pageProps from 'docs/src/pages/company/branding/branding.md?@mui/markdown';
 
+// sopme comment
 export default function Page() {
   return <TopLayoutCareers {...pageProps} />;
 }
