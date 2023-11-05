2912ca8021157d9fc30e2581d86883eb4b63befe
Red <143008303+codemasterred@users.noreply.github.com>
2023-11-05 08:04:01 +0000

    Update TextField.js
    Signed-off-by: Red <143008303+codemasterred@users.noreply.github.com>

--- packages/mui-material/src/TextField/TextField.js
+++ packages/mui-material/src/TextField/TextField.js
@@ -15,7 +15,7 @@ import FormHelperText from '../FormHelperText';
 import Select from '../Select';
 import { getTextFieldUtilityClass } from './textFieldClasses';
 
-//comm
+//comme
 const variantComponent = {
   standard: Input,
   filled: FilledInput,
